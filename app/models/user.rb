7e96c07fdacfb5a76e7ef5dc4351bb76e5855e17
丸茂沙織 <marumo@marumonoMBP.local>
2018-05-25 21:21:40 +0900

    disp_progで質問

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   mount_uploader :image, PictureUploader
 
+  #処理をモデル側で行う
+  # food2参照    コントローラ側で引数指定したを受け取って使用
+    def disp_prog(programming)
+      if programming.nil?
+        "NO DATA"
+      else
+        programming.join(、)
+      end
+
+    end
+
+
 end
