af0222b274fa426ed685acbc1426c0bfe43ed3f8
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-06-16 20:47:50 +0900

    modify routes of users

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,21 @@ Rails.application.routes.draw do
 
   #bootstrapの導入
   root to: 'users#index'
+  resources :users
   # get 'users/index'
-  get '/user/index', to: 'users#index'
-
-
-  get "/user/new", to: "users#new"
-  post '/user/new', to: 'users#create'
-
-  get '/users/:id/edit', to: 'users#edit'
-  #url => users/1/edit
-  #=> users/:id/edit の :id に代入される
-  #=> :id => params[:id]
-
-  patch '/users/:id', to: 'users#update'
-  delete '/users/:id', to: 'users#destroy'
+  # get '/user/index', to: 'users#index'
+  #
+  #
+  # get "/user/new", to: "users#new"
+  # post '/user/new', to: 'users#create'
+  #
+  # get '/users/:id/edit', to: 'users#edit'
+  # #url => users/1/edit
+  # #=> users/:id/edit の :id に代入される
+  # #=> :id => params[:id]
+  #
+  # patch '/users/:id', to: 'users#update'
+  # delete '/users/:id', to: 'users#destroy'
 
 
 end
