6272fa671baa7a10fa77fcef43f4397e0c971d58
marim0ss <piyomaru.bunbun@gmail.com>
2018-06-24 16:48:54 +0900

    resources posts,Index test, Add provide

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get '/posts/index', to: 'posts#index'
-  get '/posts/new', to: 'posts#new'
-  post '/posts/new', to: 'posts#create'
-  #
-  # #bootstrapの導入
+
+  resources :posts
+
+
   root to: 'users#index'
+
+  #resourcesを使ってルートを記述
   resources :users
+  # => index, show, new, create, edit, update, destroyのアクションに対応させることができる
+
+
+  # =>  ルートの元の記述 -------------------------------------------------------
   # # get 'users/index'
   # get '/user/index', to: 'users#index'
   #
@@ -16,13 +21,19 @@ Rails.application.routes.draw do
   # post '/user/new', to: 'users#create'
   #
   # get '/users/:id/edit', to: 'users#edit'
-  # #url => users/1/edit
-  # #=> users/:id/edit の :id に代入される
-  # #=> :id => params[:id]
+        # #url => users/1/edit
+        # #=> users/:id/edit の :id に代入される
+        # #=> :id => params[:id]
   #
   # patch '/users/:id', to: 'users#update'
   # delete '/users/:id', to: 'users#destroy'
 
-# => index, show, new, create, edit, update, destroy
+
+  # postモデル
+  # get '/posts/index', to: 'posts#index'
+  # get '/posts/new', to: 'posts#new'
+  # post '/posts/new', to: 'posts#create'
+  #
+
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Userlists App"
+  end
+
+
   test "should get index" do
+    # ルートのテスト
     get :index
     assert_response :success
+    # titleタグにUserlistsの文字があるかどうか
+    assert_select "title", "トップ | #{@base_title}"
   end
 
 end
