9cfb5920c787f28c10ace110da0f3c0b60a794d2
marim0ss <piyomaru.bunbun@gmail.com>
2018-07-04 21:16:17 +0900

    change user_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,13 @@ class PostsController < ApplicationController
     @posts = Post.where(user_id: params[:user_id])
       #params[:user_id] ...user_idのパラメータが飛んできたのを
       # where(user_id   )で探す
-    @user = User.find(params[:id])
+    @user = User.find(params[:user_id])
     #ここでもuser_idを活用してユーザーの情報を取ってくる
   end
 
   def new
     # ここでもユーザーに紐づいた投稿をしたいのでparams[:user_id]でユーザーを探す
-    @user = User.find(params[:id])
+    @user = User.find(params[:user_id])
     @post = Post.new
     #１投稿なのでpost 単数形
   end
@@ -19,7 +19,9 @@ class PostsController < ApplicationController
   def create
     #createアクションでstrong parameter のメソッドを呼び出す
     # Post.create(posts_params)
-    Post.create(user_id: params[:user_id], content: posts_params[:content], post_image_name: posts_params[:post_image_name])
+    # Post.create(user_id: params[:user_id], content: posts_params[:content], post_image_name: posts_params[:post_image_name])
+
+    Post.create(user_id: params[:user_id], content: posts_params[:content], post_image_name: posts_params[:post_image_name])
     # posts_paramsは下のストロングパラメータで定義したメソッドを呼び出している
   end
 
@@ -31,8 +33,9 @@ class PostsController < ApplicationController
     def posts_params
       params.require(:post).permit(:content, :post_image_name)
       # params.require(:post) のpostはsinmple simple_form_forで使った@postを指している
-      # 値を送信した時に、contentはpostの配下として送信されている。user_idは独立して送信されているのでpost_paramsにはしない
     end
+      #値を送信した時に、contentはpostの配下として送信されている。user_idは独立して送信されているのでpost_paramsにはしない
+
     #このように書けばストロングパラメータとして同じように記述できる
     # def user_id_params
     #   params.require(:post).permit(:user_id)
