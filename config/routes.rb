13ffcb17619dacd75241c5c6b1562eb814295cb8
marim0ss <piyomaru.bunbun@gmail.com>
2018-07-16 15:47:43 +0900

    change layout: top icon,navibar

--- config/routes.rb
+++ config/routes.rb
@@ -11,22 +11,4 @@ Rails.application.routes.draw do
   end
   # => index, show, new, create, edit, update, destroyのアクションに対応させることができる
 
-
-  # =>  ルートの元の記述 -------------------------------------------------------
-  # # get 'users/index'
-  # get '/user/index', to: 'users#index'
-  #
-  #
-  # get "/user/new", to: "users#new"
-  # post '/user/new', to: 'users#create'
-  #
-  # get '/users/:id/edit', to: 'users#edit'
-        # #url => users/1/edit
-        # #=> users/:id/edit の :id に代入される
-        # #=> :id => params[:id]
-  #
-  # patch '/users/:id', to: 'users#update'
-  # delete '/users/:id', to: 'users#destroy'
-
-
 end
