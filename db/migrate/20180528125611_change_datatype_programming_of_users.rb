f9d97decebe780ebb127675f7885487d52bbb120
marim0ss <piyomaru.bunbun@gmail.com>
2018-07-17 22:25:34 +0900

    USING CAST

--- db/migrate/20180528125611_change_datatype_programming_of_users.rb
+++ db/migrate/20180528125611_change_datatype_programming_of_users.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeProgrammingOfUsers < ActiveRecord::Migration
   def change
-    change_column :users, :programming, :text, USING(programming::text[])
+    change_column :users, :programming, 'text USING CAST(programming AS text[])'
   end
 end
