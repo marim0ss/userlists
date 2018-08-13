7fb3265a5a79586936207c0724a01c099c8c0d52
marim0ss <piyomaru.bunbun@gmail.com>
2018-08-13 15:10:19 +0900

    name_cont=>成功, age_contで検索できてない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @posts = Post.all
 
   # データ検索ができる。commentにデータがない場合はall検索となる
-    if params[:search_name || :search_age].present?
+    if params[:name_cont || :age_cont].present?
       user_search = UserSearch.new(params_user_search)
       @users = user_search.execute
     end
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
     # {:programming => []}  配列を登録できるように指定
 
     def params_user_search
-      params.permit(:search_name, :search_age)
+      params.permit(:name_cont, :age_cont)
     end
 end
--- app/models/user_search.rb
+++ app/models/user_search.rb
@@ -1,11 +1,11 @@
 class UserSearch < ApplicationRecord
   include ActiveModel::Model
 
-  attr_accessor :search_name, :search_age
+  attr_accessor :name_cont, :age_cont
 
   # 検索用のメソッド
   def execute
-    User.ransack(name_eq: @search_name, age_eq: @search_age).result
+    User.search(name_cont: @name_cont, age_cont: @age_cont).result
   end
   # ※userモデルに検索用のメソッドを実装する場合、インスタンス変数の名前がテーブルのカラム名と同じ値になると、データが登録できない状態になるので注意
 end
